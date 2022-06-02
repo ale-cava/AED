693cc169b45f247f18b69af2914dfad20cb0bafd
ale-cava <102269252+ale-cava@users.noreply.github.com>
2022-06-02 10:42:52 -0300

    update

--- 01-EjemploTipos/EjemploTipos.cpp
+++ 01-EjemploTipos/EjemploTipos.cpp
@@ -1,13 +1,34 @@
-#include <iostream>
+#include <string>
 #include <cassert>
 
-int main() {
-    assert(2 == 1 + 1);
-    assert('a' == 'a');
-    assert('a' != 'b');
-    assert(10 > 7);
+int main() 
+{
+    //bool
     assert(true);
     assert(not false);
-    assert(-2 == -2);
-    assert(3.141592653589793 == 3.141592653589793);
+    
+    //char
+    assert('a' != 'b');
+    assert('d' + 'q' == 0xD5);
+    assert( 'C' - 'A' == 2);
+    assert('C' - 2 == 'A');
+    assert('C' + 2 == 'E');
+    assert('C' - 2 == 65);
+    assert('C' - 2 == 65 and 'C' - 2 == 'A');
+
+    //int
+    assert(123 == 123);
+    assert(134 + 4 == 138);
+    assert(9999999 > 9999998);
+    
+    //flotante
+    assert(0.23 > -1.0); 
+    assert(3E2 == 3e2 );
+    assert(3E2 == 3*10*10 );
+    assert(3E2 == 300.0 );
+    assert(3E6 == 3000000.0 );
+    assert(3E6 == 3'000'000.0 );
+    assert(2.0 + 1 == 3.0);
+    assert(1.0);
+
 }
